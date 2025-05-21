331b0d19faaf9f171514d29207711d2a9b5e44dc
Richard Leyshon <richard.leyshon@justice.gov.uk>
2025-05-21 14:41:24 +0100

    refactor: Tests use fixtures

--- tests/test_fizzbuzz.py
+++ tests/test_fizzbuzz.py
@@ -1,17 +1,42 @@
+import pytest
 from src.fizzbuzz import fizzbuzz
 
-def test_fizzbuzz_fizzes():
-    assert fizzbuzz(3) == "fizz", "Should have fizzed."
-    assert fizzbuzz(6) == "fizz", "Should have fizzed."
 
-def test_fizzbuzz_buzzes():
-    assert fizzbuzz(5) == "buzz", "Should have buzzed."
-    assert fizzbuzz(10) == "buzz", "Should have buzzed."
+@pytest.fixture
+def fizzy_numbers():
+    return [3, 6]
 
-def test_fizzbuzz_fizzbuzzes():
-    assert fizzbuzz(15) == "fizzbuzz", "Should have fizzbuzzed."
-    assert fizzbuzz(30) == "fizzbuzz", "Should have fizzbuzzed."
 
-def test_fizzbuzz_numbers():
-    assert fizzbuzz(1) == "1", "Should return number as string."
-    assert fizzbuzz(2) == "2", "Should return number as string."
+@pytest.fixture
+def buzzy_numbers():
+    return [5, 10]
+
+
+@pytest.fixture
+def fizzbuzzy_numbers():
+    return [15, 30]
+
+
+@pytest.fixture
+def quiet_numbers():
+    return [1, 2, 4, 7, 8]
+
+
+def test_fizzbuzz_fizzes(fizzy_numbers):
+    for number in fizzy_numbers:
+        assert fizzbuzz(number) == "fizz", f"Should have fizzed for {number}"
+
+
+def test_fizzbuzz_buzzes(buzzy_numbers):
+    for number in buzzy_numbers:
+        assert fizzbuzz(number) == "buzz", f"Should have buzzed for {number}"
+
+
+def test_fizzbuzz_fizzbuzzes(fizzbuzzy_numbers):
+    for number in fizzbuzzy_numbers:
+        assert fizzbuzz(number) == "fizzbuzz", f"Should have fizzbuzzed for {number}"
+
+
+def test_fizzbuzz_numbers(quiet_numbers):
+    for number in quiet_numbers:
+        assert fizzbuzz(number) == str(number), f"Should return {number} as string"
