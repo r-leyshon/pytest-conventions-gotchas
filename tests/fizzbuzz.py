6c3b4df8eaa6e131dc3f1f3e15927fa4b59e1a64
Richard Leyshon <richard.leyshon@justice.gov.uk>
2025-05-21 09:36:34 +0100

    test: Module naming conventions

---
+++ tests/fizzbuzz.py
@@ -0,0 +1,17 @@
+from src.fizzbuzz import fizzbuzz
+
+def test_fizzbuzz_fizzes():
+    assert fizzbuzz(3) == "fizz", "Should have fizzed."
+    assert fizzbuzz(6) == "fizz", "Should have fizzed."
+
+def test_fizzbuzz_buzzes():
+    assert fizzbuzz(5) == "buzz", "Should have buzzed."
+    assert fizzbuzz(10) == "buzz", "Should have buzzed."
+
+def test_fizzbuzz_fizzbuzzes():
+    assert fizzbuzz(15) == "fizzbuzz", "Should have fizzbuzzed."
+    assert fizzbuzz(30) == "fizzbuzz", "Should have fizzbuzzed."
+
+def test_fizzbuzz_numbers():
+    assert fizzbuzz(1) == "1", "Should return number as string."
+    assert fizzbuzz(2) == "2", "Should return number as string."
