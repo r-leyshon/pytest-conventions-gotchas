a2874cefc689302c3f7388508d9b262ab5854623
Richard Leyshon <richard.leyshon@justice.gov.uk>
2025-05-21 13:29:13 +0100

    refactor: Use a test class

--- tests/test_fizzbuzz.py
+++ tests/test_fizzbuzz.py
@@ -1,17 +1,19 @@
 from src.fizzbuzz import fizzbuzz
 
-def test_fizzbuzz_fizzes():
-    assert fizzbuzz(3) == "fizz", "Should have fizzed."
-    assert fizzbuzz(6) == "fizz", "Should have fizzed."
+class TestFizzbuzz:
+    """Grouping all tests for function fizzbuzz."""
+    def test_fizzbuzz_fizzes(self):
+        assert fizzbuzz(3) == "fizz", "Should have fizzed."
+        assert fizzbuzz(6) == "fizz", "Should have fizzed."
 
-def test_fizzbuzz_buzzes():
-    assert fizzbuzz(5) == "buzz", "Should have buzzed."
-    assert fizzbuzz(10) == "buzz", "Should have buzzed."
+    def test_fizzbuzz_buzzes(self):
+        assert fizzbuzz(5) == "buzz", "Should have buzzed."
+        assert fizzbuzz(10) == "buzz", "Should have buzzed."
 
-def test_fizzbuzz_fizzbuzzes():
-    assert fizzbuzz(15) == "fizzbuzz", "Should have fizzbuzzed."
-    assert fizzbuzz(30) == "fizzbuzz", "Should have fizzbuzzed."
+    def test_fizzbuzz_fizzbuzzes(self):
+        assert fizzbuzz(15) == "fizzbuzz", "Should have fizzbuzzed."
+        assert fizzbuzz(30) == "fizzbuzz", "Should have fizzbuzzed."
 
-def test_fizzbuzz_numbers():
-    assert fizzbuzz(1) == "1", "Should return number as string."
-    assert fizzbuzz(2) == "2", "Should return number as string."
+    def test_fizzbuzz_numbers(self):
+        assert fizzbuzz(1) == "1", "Should return number as string."
+        assert fizzbuzz(2) == "2", "Should return number as string."
