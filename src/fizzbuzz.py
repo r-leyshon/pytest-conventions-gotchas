b5ea691ec3f9317c520491e4cd76c289a1f8efe3
Richard Leyshon <richard.leyshon@justice.gov.uk>
2025-05-22 11:35:25 +0100

    chore: Rm old files

--- src/fizzbuzz.py
+++
@@ -1,8 +0,0 @@
-def fizzbuzz(n):
-    if n % 3 == 0 and n % 5 == 0:
-        return "fizzbuzz"
-    if n % 3 == 0:
-        return "fizz"
-    if n % 5 == 0:
-        return "buzz"
-    return str(n)
